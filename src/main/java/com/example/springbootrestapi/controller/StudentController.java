776c9a6a9fc2b19d3ff85bed92149381ab654f8c
Crisnzx <cristovaon13@gmail.com>
2023-01-13 00:52:25 -0300

    feat: Add RequestMapping

--- src/main/java/com/example/springbootrestapi/controller/StudentController.java
+++ src/main/java/com/example/springbootrestapi/controller/StudentController.java
@@ -10,30 +10,31 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
+@RequestMapping("students")
 public class StudentController {
     private List<Student> students = new ArrayList<Student>();
     private int id = 0;
 
-    @GetMapping("student/path-variables/{first-name}/{last-name}")
+    @GetMapping("path-variables/{first-name}/{last-name}")
     public ResponseEntity<Student> makeStudentWithPathVariables(@PathVariable("first-name") String firstName, @PathVariable("last-name") String lastName) {
         Student student = new Student(this.id++, firstName, lastName);
         this.students.add(student);
         return new ResponseEntity<>(student, HttpStatus.CREATED);
     }
 
-    @GetMapping("student/query-params")
+    @GetMapping("query-params")
     public ResponseEntity<Student> makeStudentWithQueryParams(@RequestParam String firstName, @RequestParam(required = false) String lastName) {
         Student student = new Student(this.id++, firstName, lastName);
         this.students.add(student);
         return new ResponseEntity<>(student, HttpStatus.CREATED);
     }
 
-    @GetMapping("students")
+    @GetMapping
     public ResponseEntity<List<Student>> getStudents() {
         return ResponseEntity.ok(this.students);
     }
 
-    @GetMapping("students/{id}")
+    @GetMapping("{id}")
     public ResponseEntity<Student> getStudent(@PathVariable int id) {
         try {
         Student student = this.students.get(id);
@@ -43,14 +44,14 @@ public class StudentController {
         }
     }
 
-    @PostMapping("students")
+    @PostMapping
     public ResponseEntity<Student> createStudent(@RequestBody Student student) {
         student.setId(this.id++);
         this.students.add(student);
         return new ResponseEntity<>(student, HttpStatus.CREATED);
     }
 
-    @PutMapping("students/{id}")
+    @PutMapping("{id}")
     public ResponseEntity<Student> updateStudent(@PathVariable int id, @RequestBody Student student) {
         try {
             this.students.set(id, student);
@@ -61,7 +62,7 @@ public class StudentController {
         }
     }
 
-    @DeleteMapping("students/{id}")
+    @DeleteMapping("{id}")
     public ResponseEntity<String> deleteStudent(@PathVariable int id) {
         try {
             Student student = this.students.stream().filter(s -> s.getId() == id).collect(Collectors.toList()).get(0);
